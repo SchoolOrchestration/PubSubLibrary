076c885326f50d713474f31f38d0c3d9a9b11603
Bradley Stuart Kirton <bradleykirton@gmail.com>
2019-06-19 16:11:37 +0200

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="school_pubsub",
-    version="0.6.6",
+    version="0.7.1",
     description="An abstracted interface for pub/sub between microservices",
     url="https://github.com/AppointmentGuru/PubSubGuru",
     author="Christo Crampton",
